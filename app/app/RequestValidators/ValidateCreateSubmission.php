d5988017f868f8d1cf7ba445d6cbc24886158577
azonedev <abdullah914115@gmail.com>
2023-08-21 06:33:42 +0600

    Implementing request data validation

--- app/app/RequestValidators/ValidateCreateSubmission.php
+++ app/app/RequestValidators/ValidateCreateSubmission.php
@@ -9,25 +9,52 @@ class ValidateCreateSubmission
     {
         $errors = [];
 
-        // Amount required & only numbers
-        if(!isset($data['amount'])){
-            $errors['amount'] = "Amount is required.";
-        }else{
-            if (!is_numeric($data['amount'])) {
-                $errors['amount'] = "Amount must be a valid number.";
-            }
+        // Amount (only numbers)
+        if (!isset($data['amount']) || !is_numeric($data['amount'])) {
+            $errors['amount'] = "Amount must be a valid number.";
         }
 
-        // Validate Buyer (only text, spaces, and numbers, max 20 characters)
-        if (!preg_match('/^[a-zA-Z0-9\s]{1,20}$/', $data['buyer'])) {
-            $errors['buyer'] = "Buyer must be alphanumeric and not exceed 20 characters.";
+        // Buyer (only text, spaces, and numbers, max 20 characters)
+        if (!isset($data['buyer']) || !preg_match('/^[a-zA-Z0-9\s]{1,255}$/', $data['buyer'])) {
+            $errors['buyer'] = "Buyer must be alphanumeric and not exceed 255 characters.";
         }
 
-        // Validate Receipt ID (only text, max 20 characters)
-        if (!preg_match('/^[a-zA-Z]{1,20}$/', $data['receipt_id'])) {
-            $errors['receipt_id'] = "Receipt ID must contain only text and not exceed 20 characters.";
+        // Receipt ID (only text and numbers, max 20 characters)
+        if (!isset($data['receipt_id']) || !preg_match('/^[a-zA-Z0-9]{1,20}$/', $data['receipt_id'])) {
+            $errors['receipt_id'] = "Receipt ID must contain only text and numbers and not exceed 20 characters.";
+        }
+
+        // Items (only text, max 255 characters)
+        if (!isset($data['items']) || !preg_match('/^[a-zA-Z\s]{1,255}$/', $data['items'])) {
+            $errors['items'] = "Items must contain only text and not exceed 255 characters.";
+        }
+
+        // Buyer Email (valid email address, max 50 characters)
+        if (!isset($data['buyer_email']) || !filter_var($data['buyer_email'], FILTER_VALIDATE_EMAIL) || strlen($data['buyer_email']) >= 50) {
+            $errors['buyer_email'] = "Buyer Email must be a valid email address and not exceed 50 characters.";
+        }
+
+        // Note
+         if(!isset($data['note'])){
+            $errors['note'] = "Note field is required";
+        }
+
+        // City (only text, max 20 characters)
+        if (!isset($data['city']) || !preg_match('/^[a-zA-Z\s]{1,20}$/', $data['city'])) {
+            $errors['city'] = "City must contain only text and not exceed 20 characters.";
+        }
+
+        // Phone (only numbers, max 20 characters)
+        if (!isset($data['phone']) || !preg_match('/^[0-9]{1,20}$/', $data['phone'])) {
+            $errors['phone'] = "Phone must contain only numbers and not exceed 20 characters.";
+        }
+
+        // Entry by (only numbers, max 10 characters)
+        if (!isset($data['entry_by']) || !is_numeric($data['amount'])) {
+            $errors['entry_by'] = "Entry by must be a valid number.";
         }
 
         return $errors;
     }
+
 }
